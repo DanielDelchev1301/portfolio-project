08be85e3f4e04ddc00603d18791355060a3d903f
Daniel Delchev <kdanieldelchev1301@abv.bg>
2022-10-14 01:41:29 +0300

    finish contact page
    (functionality, styling, and refactoring)

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,7 +1,24 @@
+const { sendEmail } = require('../services/emailService');
+const { getErrorMessage } = require('../utils/errorHelper');
+
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
     res.render('contact');
+});
+
+router.post('/', async (req, res) => {
+    const emailData = req.body;
+
+    try {
+        const sendedEmail = await sendEmail(emailData);
+        console.log(sendedEmail);
+        const email = sendedEmail.email;
+
+        res.render('contact', {email});
+    } catch (error) {
+        res.render('contact', {error: getErrorMessage(error)});
+    }
 });
 
 module.exports = router;
