67f07337880c83aefd3cebdad963c7083be79075
Daniel Delchev <kdanieldelchev1301@abv.bg>
2022-10-14 00:26:01 +0300

    add and style contact page

--- routes.js
+++ routes.js
@@ -3,10 +3,11 @@ const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const aboutController = require('./controllers/aboutController');
 const projectsController = require('./controllers/projectsController');
-const { route } = require('./controllers/projectsController');
+const contactController = require('./controllers/contactController');
 
 router.use('/', homeController);
 router.use('/about', aboutController);
-router.use('/projects', projectsController)
+router.use('/projects', projectsController);
+router.use('/contact', contactController);
 
 module.exports = router;
