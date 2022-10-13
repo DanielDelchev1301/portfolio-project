b021c8e784391fb361eaae3a26741ccc334eb4e4
Daniel Delchev <kdanieldelchev1301@abv.bg>
2022-10-13 15:48:21 +0300

    add project page
    add id to section in about page and finish about page, the one thing that left is to figure out what to write about myself

--- routes.js
+++ routes.js
@@ -2,8 +2,11 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
 const aboutController = require('./controllers/aboutController');
+const projectsController = require('./controllers/projectsController');
+const { route } = require('./controllers/projectsController');
 
 router.use('/', homeController);
 router.use('/about', aboutController);
+router.use('/projects', projectsController)
 
 module.exports = router;
