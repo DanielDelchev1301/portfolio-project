597e2cab573742c188e7168010ac2b976726b996
Daniel Delchev <kdanieldelchev1301@abv.bg>
2022-10-14 11:25:55 +0300

    add certificates page and finish other little stuff

--- routes.js
+++ routes.js
@@ -4,10 +4,12 @@ const homeController = require('./controllers/homeController');
 const aboutController = require('./controllers/aboutController');
 const projectsController = require('./controllers/projectsController');
 const contactController = require('./controllers/contactController');
+const certificatesController = require('./controllers/certificatesController');
 
 router.use('/', homeController);
 router.use('/about', aboutController);
 router.use('/projects', projectsController);
 router.use('/contact', contactController);
+router.use('/certificates', certificatesController);
 
 module.exports = router;
